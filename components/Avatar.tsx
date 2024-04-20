a40239f8648d35449bdd72a64ca748b05c016f89
aaron <aaronlimchengkiat@gmail.com>
2024-04-20 22:57:30 +0800

    Update Avatar component to include unoptimized image with reduced quality

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,14 @@ import { Skeleton } from "./ui/skeleton";
 export default function Avatar() {
   return (
     <div className="relative h-12 w-12">
-      <Image src="/aaron.jpeg" alt="Aaron" fill className="rounded-full" />
+      <Image
+        src="/aaron.jpeg"
+        alt="Aaron"
+        fill
+        className="rounded-full"
+        unoptimized
+        quality={75}
+      />
     </div>
   );
 }
