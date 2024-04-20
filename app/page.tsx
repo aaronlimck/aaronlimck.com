4b2e1ae5b2fa3d29debea664e2ba04f42c6a00f1
aaron <aaronlimchengkiat@gmail.com>
2024-04-21 00:07:39 +0800

    Update metadata description in layout.tsx and add Umami analytics integration

--- app/page.tsx
+++ app/page.tsx
@@ -7,66 +7,76 @@ import {
   GitHubLogoIcon,
   LinkedInLogoIcon,
 } from "@radix-ui/react-icons";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="mx-auto w-full max-w-2xl space-y-8 pt-10">
-      <section className="space-y-6 px-4">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-row items-center space-x-3">
-            <div className="relative h-12 w-12">
-              <Suspense fallback={<Avatar.Skeleton />}>
-                <Avatar />
-              </Suspense>
+    <>
+      <Head>
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
+      </Head>
+
+      <main className="mx-auto w-full max-w-2xl space-y-8 pt-10">
+        <section className="space-y-6 px-4">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-row items-center space-x-3">
+              <div className="relative h-12 w-12">
+                <Suspense fallback={<Avatar.Skeleton />}>
+                  <Avatar />
+                </Suspense>
+              </div>
+
+              <div className="flex flex-col">
+                <span className="font-medium">Aaron Lim</span>
+                <span className="text-sm text-muted-foreground">
+                  Aspiring Product Engineer
+                </span>
+              </div>
             </div>
 
-            <div className="flex flex-col">
-              <span className="font-medium">Aaron Lim</span>
-              <span className="text-sm text-muted-foreground">
-                Aspiring Product Engineer
-              </span>
+            <div className="flex gap-2">
+              <Link
+                target="_blank"
+                href="https://github.com/aaronlimck"
+                className={buttonVariants({ variant: "outline", size: "icon" })}
+              >
+                <GitHubLogoIcon className="h-5 w-5" />
+              </Link>
+
+              <Link
+                target="_blank"
+                href="https://www.linkedin.com/in/aaronlimchengkiat/"
+                className={buttonVariants({ variant: "outline", size: "icon" })}
+              >
+                <LinkedInLogoIcon className="h-5 w-5" />
+              </Link>
+
+              <ThemeToggle />
             </div>
           </div>
 
-          <div className="flex gap-2">
-            <Link
-              target="_blank"
-              href="https://github.com/aaronlimck"
-              className={buttonVariants({ variant: "outline", size: "icon" })}
-            >
-              <GitHubLogoIcon className="h-5 w-5" />
-            </Link>
+          <p>
+            Hi, I&apos;m Aaron, based in Singapore. My interests lie in
+            designing, managing, and developing products that solve real-world
+            problems. Let&apos;s bring those ideas to life!
+          </p>
+        </section>
 
-            <Link
-              target="_blank"
-              href="https://www.linkedin.com/in/aaronlimchengkiat/"
-              className={buttonVariants({ variant: "outline", size: "icon" })}
-            >
-              <LinkedInLogoIcon className="h-5 w-5" />
-            </Link>
+        <section className="flex flex-col">
+          <div className="mb-4 px-4 font-medium text-primary">Projects</div>
 
-            <ThemeToggle />
-          </div>
-        </div>
-
-        <p>
-          Hi, I&apos;m Aaron, based in Singapore. My interests lie in designing,
-          managing, and developing products that solve real-world problems.
-          Let&apos;s bring those ideas to life!
-        </p>
-      </section>
-
-      <section className="flex flex-col">
-        <div className="mb-4 px-4 font-medium text-primary">Projects</div>
-
-        <Suspense fallback={<ProjectItem.Skeleton />}>
-          <ProjectItem />
-          <ProjectItem />
-        </Suspense>
-      </section>
-    </main>
+          <Suspense fallback={<ProjectItem.Skeleton />}>
+            <ProjectItem />
+            <ProjectItem />
+          </Suspense>
+        </section>
+      </main>
+    </>
   );
 }
